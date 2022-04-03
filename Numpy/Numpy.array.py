382c845898d7222250e938ca3c20cbad6cdc5914
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2022-04-03 13:53:05 +0200

    Kurs Python Tomasz Pielecki

---
+++ Numpy/Numpy.array.py
@@ -0,0 +1,18 @@
+import numpy as np
+x=np.arange(20)
+b=np.linspace(0,10,6)
+print(x)
+print(b)
+
+y=np.zeros((1,5,6))
+print(y)
+
+z=np.array([[3.122,2.777,5.333],[4.222,6.333,5.321]])
+o=z+2,532
+o=z*2.5
+print(o)
+
+u=np.arange(2,10,3)
+
+print("from 2 to 10 step 3 = ",u) #2-5-8
+
